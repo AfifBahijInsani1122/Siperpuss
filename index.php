7c28ce6da1e57305adfe6fe089d67e3bfa5ed61e
AfifBahijInsani1122 <63805986+AfifBahijInsani1122@users.noreply.github.com>
2020-04-17 00:18:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include 'aset/header.php';
+ ?>
+ <div class="container">
+   <div class="row mt-4">
+     <div class="col-md">
+       <h2></i>Dashbord</h2>
+       <hr class="bg-light">
+     </div>
+   </div>
+ </div>
+     <div class="container">
+        <div class="row mt-4">
+   </div>
+ </div>
+ <div class="row">
+
+ <div class="col-md-4">
+                <div class="card bg-danger" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">jumlah buku</h5>
+                        <p class="card-text" style="font-size:60px">350</p>
+                        <a href="#" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                            <div class="card bg-warning" style="width: 18rem;">
+                                <div class="card-body text-white">
+                                    <h5 class="card-title">jumlah anggota</h5>
+                                    <p class="card-text" style="font-size:60px">614</p>
+                                    <a href="#" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                                       <div class="card bg-primary" style="width: 18rem;">
+                                           <div class="card-body text-white">
+                                               <h5 class="card-title">jumlah buku</h5>
+                                               <p class="card-text" style="font-size:60px">350</p>
+                                               <a href="#" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                                           </div>
+                                       </div>
+                                   </div>
+</div>
+
+ <?php
+include 'aset/footer.php';
+  ?>
